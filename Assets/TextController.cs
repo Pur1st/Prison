2fd7a2d5f3821137180b1fb19a19852e6d98aabd
Pur1st <tylerchristiaens@gmail.com>
2018-05-27 00:40:54 -0700

    updated and expanded room text-text

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -6,7 +6,7 @@ public class TextController : MonoBehaviour {
 
 	public Text text;
 	private enum States {
-		cell, sheets_0, grate, lock_0, sheets_1, cell_grate, lock_1,
+		cell, cell_again, sheets_0, grate, lock_0, sheets_1, cell_grate, lock_1,
 		corridor_0, corridor_1, corridor_2, corridor_3, stairs_0, 
 		stairs_1, stairs_2, floor, closet_door, in_closet, courtyard   
 		};
@@ -27,6 +27,9 @@ public class TextController : MonoBehaviour {
 		switch (myState) {
 			case States.cell:
 				cell();
+				break;
+			case States.cell_again:
+				cell_again();
 				break;
 			case States.sheets_0:
 				sheets_0();
@@ -89,56 +92,13 @@ public class TextController : MonoBehaviour {
 					" A warm and fowl breeze blows from underneath the door. " +  
 					" You have no memory of this place..." +
 					" While struggling to get to your feet you notice" + 
-					" there are some sheets on the floor" + 
-					" covering a broken grate." +
+					" Some sheets crumpled up on the floor." + 
+					" Covering a broken grate." +
 					" You step over to check the door...\n\n" +
-					" Press (S) to sniff the sheets, (G) to check the Grate and (L) to check the lock" ; 
+					" Press (S) to examine the sheets, (G) to check the grate and (L) to check the lock" ; 
 		if (Input.GetKeyDown(KeyCode.S)) 			{myState = States.sheets_0;}
 		else if (Input.GetKeyDown(KeyCode.G)) 		{myState = States.grate;}
 		else if (Input.GetKeyDown(KeyCode.L)) 		{myState = States.lock_0;}
-	}
-	
-	void sheets_0 () {
-		text.text = "They smell like they havent been washed in ages." + 
-					" you lurch away when you see the blood stains. " +  
-					" What the hell happened here!?\n\n" +
-					" Press (R) to return to roaming your cell" ; 
-		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell;}
-	}
-	
-	void sheets_1 () {
-		text.text = "Using the sharp metal from the grate you tear a strip off the sheet" + 
-					" to use as a bandana to soak up your sweat. Good thing red" +  
-					" is your favorite color. This wont help much with that lock though...\n\n" +
-					" Press (R) to return to roaming your cell" ; 
-		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell_grate;}
-	}
-	
-	void lock_0 () {
-		text.text = "This lock can't be opened without a key" + 
-					" If only there was a way to get passed it without one...\n" +  
-					" WWJD? \n\n" +
-					" Press (R) to return to roaming your cell" ; 
-		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell;}
-	}
-	
-	void lock_1 () {
-		text.text = "Using the sharp metal from the grate you pick the lock" + 
-					" Thanks for nothing Jesus...\n\n"  +  
-					" Press (0) to open your cell" +
-					" or press (R) and rot for eternity just because." ; 
-		if (Input.GetKeyDown(KeyCode.O)) 			{myState = States.corridor_0;}
-		else if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.cell_grate;}
-	}
-	
-	void grate () {
-		text.text = "You notice a sharp piece of metal broken off the grate " + 
-					" broken and laying on the floor" + 
-					" I wonder if I could use that...\n\n"  +  
-					" Press (T) to take the Sharp piece of grate" +
-					" or press (R) and return to your cell." ; 
-		if (Input.GetKeyDown(KeyCode.T)) 			{myState = States.cell_grate;}
-		else if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.cell;}
 	}
 	
 	void cell_grate () {
@@ -150,9 +110,69 @@ public class TextController : MonoBehaviour {
 		else if (Input.GetKeyDown(KeyCode.L)) 		{myState = States.lock_1;}
 	}
 	
+	void cell_again () {
+		text.text = "You are still in the cell." + 
+					" You can see a faint light underneath the door. " +  
+					" You imagine what horrors are in this place..." +
+					" There has to be a way out..." + 
+					" What's the deal with those sheets." + 
+					" This is just grate..." +
+					" \n\n" +
+					" Press (S) to examine the sheets, (G) to check the grate" ; 
+		if (Input.GetKeyDown(KeyCode.S)) 			{myState = States.sheets_0;}
+		else if (Input.GetKeyDown(KeyCode.G)) 		{myState = States.grate;}
+	}
+	void sheets_0 () {
+		text.text = "They smell like they havent been washed in ages." + 
+					" Why would you smell them? " +
+					" You lurch away when you see blood stains. " +  
+					" What the hell happened here? " +
+					" \n\n " +
+					" Press (R) to return to your cell" ; 
+		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell_again;}
+	}
+	
+	void sheets_1 () {
+		text.text = "Why bother with the sheets?" + 
+					"You can smell them just fine from where you are." +
+					"\n\n" +
+					" Press (R) to return to roaming your cell" ; 
+		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell_grate;}
+	}
+	
+	void lock_0 () {
+		text.text = "This lock can't be opened without a key" + 
+					" If only there was a way to get passed it without one..." +  
+					" \n\n" +
+					" Press (R) to return to roaming your cell" ; 
+		if (Input.GetKeyDown(KeyCode.R)) 			{myState = States.cell_again;}
+	}
+	
+	void lock_1 () {
+		text.text = "Perhaps you could use the sharp piece of grate" + 
+					" to pick the lock and quietly sneak through?" +
+					" Or maybe sit here stay awhile and listen? "  + 
+					" \n\n" + 
+					" Press (0) to open your cell" +
+					" or press (R) and rot for eternity just because." ; 
+		if (Input.GetKeyDown(KeyCode.O)) 			{myState = States.corridor_0;}
+		else if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.cell_grate;}
+	}
+	
+	void grate () {
+		text.text = "You notice a sharp piece of metal broken off the grate " + 
+					" broken and laying on the floor." + 
+					" I wonder if I could use that? \n\n"  +  
+					" Press (T) to take the Sharp piece of grate" +
+					" or press (R) and return to your cell." ; 
+		if (Input.GetKeyDown(KeyCode.T)) 			{myState = States.cell_grate;}
+		else if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.cell_again;}
+	}
+	
+	
 	void corridor_0 () {
-		text.text = "Sweet glory-hole, You are free..." + 
-					" from that cell... not out of the fire yet." +
+		text.text = "Out of the frying pan " + 
+					" ... not out of the fire yet." +
 					" You see a set of stairs but hear strange sounds coming from that direction"  +
 					" Ahead you see a closet but can also spot and object laying on the floor" +
 					" You take a moment to decide...\n\n" +  
@@ -163,12 +183,12 @@ public class TextController : MonoBehaviour {
 	}
 	
 	void corridor_1 () {
-		text.text = "Back in the corridor You spot the same object on the floor..." + 
+		text.text = "You spot the same object on the floor..." + 
 					" Now that you think about it the closet door is." +
 					" still there too.."  +
 					" You take a moment to decide\n\n" +  
 					" Press (S) to go upstairs, (C) to check the closet and (F) to check floor" ; 
-		if (Input.GetKeyDown(KeyCode.S)) 				{myState = States.stairs_1;}
+		if (Input.GetKeyDown(KeyCode.S)) 				{myState = States.stairs_2;}
 		else if (Input.GetKeyDown(KeyCode.C)) 			{myState = States.closet_door;}
 		else if (Input.GetKeyDown(KeyCode.F)) 			{myState = States.in_closet;}	
 	}
@@ -208,20 +228,17 @@ public class TextController : MonoBehaviour {
 	void stairs_1 () {
 		text.text = "Why am I back here..." + 
 				    " It is waaaay to scary...\n\n" +
-					" Press (R) to go upstairs, (C) to check the closet and (F) to check floor" ; 
-		if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.cell;}
-		else if (Input.GetKeyDown(KeyCode.C)) 			{myState = States.cell;}
-		else if (Input.GetKeyDown(KeyCode.F)) 			{myState = States.cell;}	
+					" Press (R) return to the corridor";
+		if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.corridor_1;}
 	}
 	
 	void stairs_2 () {
 		text.text = "For Christ sake I am not going up there." + 
 					" I'm not wearing my shitting pants!" +
-					" You take a moment to decide\n\n" +  
+					" \n\n" +  
 					" Press (R) to go back to the corridor" ; 
-		if (Input.GetKeyDown(KeyCode.S)) 				{myState = States.corridor_2;}
-		else if (Input.GetKeyDown(KeyCode.C)) 			{myState = States.cell;}
-		else if (Input.GetKeyDown(KeyCode.F)) 			{myState = States.cell;}
+		if (Input.GetKeyDown(KeyCode.R)) 				{myState = States.corridor_1;}
+		
 	}
 	
 	void floor () {
