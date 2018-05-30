e3812284b3afab3f01ab0f7b6f12078e98d279e0
Andrew Bates <andrewbatesubc@gmail.com>
2018-05-29 23:01:54 -0700

    Refactored to use new transition logic

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Transition {
+    [SerializeField]
+    private KeyCode key;
+    [SerializeField]
+    private GameObject nextRoom;
+
+    public KeyCode GetKey() {
+        return key;
+    }
+
+    public GameObject GetNextRoom() {
+        return nextRoom;
+    }
+}
---
+++ Assets/Scripts/TransitionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionManager : MonoBehaviour {
+    [SerializeField]
+    private List<Transition> transitions;
+
+	void Update () {
+        HandleTransition();
+	}
+
+    void HandleTransition() {
+        foreach (Transition transition in transitions)
+        {
+            if (Input.GetKeyDown(transition.GetKey()))
+            {
+                GameObject nextRoom = Instantiate(transition.GetNextRoom(), transform.parent);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
